af56fa736fdeb320eb0a7793400896ef3161f4f9
BKLemonTea <jyf1128@naver.com>
2020-08-31 22:31:07 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+      id: 1,
+      title: "Video awesome",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 1,
+        name: "LemonTea",
+        email: "Test@gmail.com"
+      }
+    },
+    {
+      id: 2,
+      title: "Video super",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 1,
+        name: "LemonTea",
+        email: "Test@gmail.com"
+      }
+    },
+    {
+      id: 3,
+      title: "Video nice",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 1,
+        name: "LemonTea",
+        email: "Test@gmail.com"
+      }
+    },
+    {
+      id: 4,
+      title: "Video perfect",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 1,
+        name: "LemonTea",
+        email: "Test@gmail.com"
+      }
+    }
+  ];
