7d63d4a0597fdea665cdaab893a0e34b0b598655
BKLemonTea <jyf1128@naver.com>
2020-08-31 22:57:08 +0900

    Home Controller part Two

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
       title: "Video awesome",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 1,
         name: "LemonTea",
@@ -16,7 +16,7 @@ export const videos = [
       title: "Video super",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 1,
         name: "LemonTea",
@@ -28,7 +28,7 @@ export const videos = [
       title: "Video nice",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 1,
         name: "LemonTea",
@@ -40,7 +40,7 @@ export const videos = [
       title: "Video perfect",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 1,
         name: "LemonTea",
